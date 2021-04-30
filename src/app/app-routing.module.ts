3e7939ebb613154931d5672bd18fc028c5e4e354
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2021-04-30 23:22:50 +0400

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home' },
   { path: 'about', component: AboutComponent },
 ];
 

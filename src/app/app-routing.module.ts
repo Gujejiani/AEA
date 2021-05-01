c3dd6cb21b119f0753d4b025f6045a60832ee0e6
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2021-05-01 08:41:38 +0400

    bug fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home' },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
 ];
 

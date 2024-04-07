c05f91e109b1e38b29707b187a067b91e94a26fc
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-04-07 13:30:37 +0400

    added script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent  implements OnInit, OnDestroy{
   imageRef = ref(storage, 'projects/')
   constructor(private http: HttpClient, private projectService: projectsService){}
   ngOnInit(){
-    console.log('Angular 16 is running!')
+    console.log('Angular 16 is running! woohoo!')
      this.projectService.startFetchingProjects()
 
   }
